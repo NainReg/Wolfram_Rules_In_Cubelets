d935379c7f12d04e7d36c8641adc2f2d8f345d83
Dan <146668550+NainReg@users.noreply.github.com>
2025-11-03 09:47:36 -0600

    Add files via upload

---
+++ Prueba30/Regla30Prueba8.c
@@ -0,0 +1,45 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+void checa(void);
+void setup() {
+    wait(1000);
+    checa();
+}
+
+void loop(){
+    wait(6000);
+    checa();
+}
+
+void checa() {
+    uint8_t aux1 = get_block_value(1370425);//
+    uint8_t aux3 = get_block_value(1410416);//
+    uint8_t aux2 = get_block_value(935673);//
+    
+    if(aux1 == 0 && aux2 == 0 && aux3 == 0){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 0 && aux3 == 1){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 0){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 1){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 0){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 0){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 1){
+        set_flashlight(0);
+        block_value = 0;
+    }
+}
---
+++ Prueba57/Regla57Prueba1.c
@@ -0,0 +1,47 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+void checa(void);
+void setup() {
+    //set_flashlight(100);
+    //block_value = 1;
+    set_flashlight(0);
+    block_value = 0;
+}
+
+void loop(){
+    wait(6000);
+    checa();
+}
+
+void checa() {
+    uint8_t aux2 = get_block_value(1365046);//
+    uint8_t aux3 = get_block_value(1457307);//
+    uint8_t aux1 = 0;//get_block_value(935512);//
+    
+    if(aux1 == 0 && aux2 == 0 && aux3 == 0){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 0 && aux3 == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 0){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 1){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 0){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 1){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 0){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 1){
+        set_flashlight(0);
+        block_value = 0;
+    }
+}
