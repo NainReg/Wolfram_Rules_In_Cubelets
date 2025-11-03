22b56566c2185081fe6c988ee501cd3be340aabb
Dan <146668550+NainReg@users.noreply.github.com>
2025-11-03 09:34:58 -0600

    Add Prueba

---
+++ viboritaPruebaB/ReglaViboritaPrueba1.c
@@ -0,0 +1,30 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    set_flashlight(100);
+    //set_drive_direction(BACKWARD);
+    //set_drive(0);
+    block_value = 1;
+    wait(1000);
+}
+
+void loop() {
+    uint8_t aux2 = get_block_value(935673);//
+    uint8_t aux1 = get_block_value(935512);//
+    
+    if(aux1 == 1 && block_value == 0){
+        set_flashlight(100);
+        block_value = 1;
+ //       set_drive(100);
+//        block_value = 1;
+    } else if(aux2 == 1 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+//        set_drive(0);
+ //       block_value = 0;
+    }
+
+    wait(1000);
+}
---
+++ viboritaPruebaB/ReglaViboritaPrueba10.c
@@ -0,0 +1,30 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    set_flashlight(0);
+    //set_drive_direction(BACKWARD);
+    //set_drive(0);
+    block_value = 0;
+    wait(1000);
+}
+
+void loop() {
+    uint8_t aux1 = get_block_value(1458486);//
+    uint8_t aux2 = get_block_value(929590);//
+    
+    if(aux1 == 1 && block_value == 0){
+        set_flashlight(100);
+        block_value = 1;
+ //       set_drive(100);
+//        block_value = 1;
+    } else if(aux2 == 1 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+//        set_drive(0);
+ //       block_value = 0;
+    }
+
+    wait(1000);
+}
