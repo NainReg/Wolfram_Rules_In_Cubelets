22656e21985ceb68f2418af2374c48a911bebf2a
Dan <146668550+NainReg@users.noreply.github.com>
2025-11-03 09:52:05 -0600

    Add files via upload

---
+++ Prueba110/Regla110Prueba32.c
@@ -0,0 +1,45 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+void checa(void);
+void setup() {
+    wait(1000);
+    checa();
+}
+
+void loop(){
+    wait(6000);
+    checa();
+}
+
+void checa() {
+    uint8_t aux1 = get_block_value(935525);//
+    uint8_t aux2 = get_block_value(1376339);//
+    uint8_t aux3 = 0;//get_block_value(74227);//
+    
+    if(aux1 == 0 && aux2 == 0 && aux3 == 0){
+        set_drive(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 0 && aux3 == 1){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 0){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 1){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 0){
+        set_drive(0);
+        block_value = 0;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 1){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 0){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 1){
+        set_drive(0);
+        block_value = 0;
+    }
+}
---
+++ Prueba60/Regla60Prueba22.c
@@ -0,0 +1,45 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+void checa(void);
+void setup() {
+    wait(3000);
+    checa();
+}
+
+void loop(){
+    wait(6000);
+    checa();
+}
+
+void checa() {
+    uint8_t aux2 = get_block_value(1376424);//
+    uint8_t aux3 = get_block_value(816343);//
+    uint8_t aux1 = get_block_value(929590);//
+    
+    if(aux1 == 0 && aux2 == 0 && aux3 == 0){
+        set_drive(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 0 && aux3 == 1){
+        set_drive(0);
+        block_value = 0;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 0){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 0 && aux2 == 1 && aux3 == 1){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 0){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 0 && aux3 == 1){
+        set_drive(100);
+        block_value = 1;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 0){
+        set_drive(0);
+        block_value = 0;
+    } else if(aux1 == 1 && aux2 == 1 && aux3 == 1){
+        set_drive(0);
+        block_value = 0;
+    }
+}
